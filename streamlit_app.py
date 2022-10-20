6056e517ca1cdd4c24c153ce06ee29c20332f0ca
dipali <dipk2563@gmail.com>
2022-10-20 18:28:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,6 +70,11 @@ def get_fruit_load_list():
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
     
+fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('thanks for adding ', fruit_choice)
+
+
+    
     
 
 
